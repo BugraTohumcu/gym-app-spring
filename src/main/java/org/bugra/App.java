862b2a817115f136fbf9844cf24574624734ee0c
Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-06-26 12:11:14 +0300

    inital commit

---
+++ src/main/java/org/bugra/App.java
@@ -0,0 +1,15 @@
+package org.bugra;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App{
+    private final static Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+
+        logger.info("Program is about to start...");
+
+
+    }
+}
