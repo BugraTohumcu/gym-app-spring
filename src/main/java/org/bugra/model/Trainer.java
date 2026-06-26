780605ed086de4ea13c72a3b7c98f2916ce11d8f
Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-06-26 16:51:58 +0300

    feat: model structure init

---
+++ src/main/java/org/bugra/model/Trainer.java
@@ -0,0 +1,8 @@
+package org.bugra.model;
+
+public class Trainer extends User {
+    private String specialization;
+
+    public String getSpecialization() { return specialization; }
+    public void setSpecialization(String specialization) { this.specialization = specialization; }
+}
