a63d24880e33aa877dadbd4876783d4d14ed9c01
Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-06-27 13:38:32 +0300

    feat: implement save, update methods and user based repo methods

---
+++ src/main/java/org/bugra/persistence/repo/TraineeRepo.java
@@ -0,0 +1,67 @@
+package org.bugra.persistence.repo;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.bugra.annotation.StorageQualifier;
+import org.bugra.enums.StorageType;
+import org.bugra.model.Trainee;
+import org.springframework.stereotype.Repository;
+
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * In-memory repository class for {@link Trainee}
+ * Inherits CRUD behaviour from {@link AbstractInMemoryRepository}, implements {@link CrudRepo} nad {@link UserBasedRepository}
+ * */
+
+@Repository
+public class TraineeRepo extends AbstractInMemoryRepository<Trainee, Long> implements CrudRepo<Trainee, Long>, UserBasedRepository {
+
+    public TraineeRepo(@StorageQualifier(StorageType.TRAINEE) Map<Long, Trainee> traineeStorage) {
+        super(traineeStorage);
+    }
+
+    @Override
+    public Trainee save(Trainee entity) {
+        if(entity == null) {
+            throw new IllegalArgumentException("Entity can not be null");
+        }
+
+        if(entity.getId() == null){
+            throw new IllegalArgumentException("Entity id should be auto generated");
+        }
+
+        // save entity into in-memory database
+        storageMap.put(entity.getId(), entity);
+        return entity;
+    }
+
+    @Override
+    public Optional<Trainee> updateById(Trainee entity) {
+        if(!existsById(entity.getId())) {
+            return Optional.empty();
+        }
+
+        // Update existing entity
+        storageMap.put(entity.getId(), entity);
+        return Optional.of(entity);
+    }
+
+    @Override
+    public boolean existsByUsername(String username) {
+        if (username == null) {
+            return false;
+        }
+        return storageMap.values().stream()
+                .anyMatch(trainee -> username.equalsIgnoreCase(trainee.getUsername()));
+    }
+
+    @Override
+    public Long getMaxId() {
+        return storageMap.keySet().stream()
+                .max(Long::compare)
+                .orElse(0L);
+    }
+
+}
