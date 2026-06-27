b0b651656df74d3f72f298c8c76b46a20d538938
Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-06-27 17:01:52 +0300

    feat: create traning repo inhertied from AbstractInMemoryRepository

---
+++ src/main/java/org/bugra/persistence/repo/TrainingRepo.java
@@ -0,0 +1,22 @@
+package org.bugra.persistence.repo;
+
+import org.bugra.annotation.StorageQualifier;
+import org.bugra.enums.StorageType;
+import org.bugra.model.Training;
+import org.springframework.stereotype.Repository;
+
+import java.util.Map;
+
+@Repository
+public class TrainingRepo extends AbstractInMemoryRepository<Training, Long> {
+
+    protected TrainingRepo(
+            @StorageQualifier(StorageType.TRAINING) Map<Long, Training> storageMap) {
+        super(storageMap);
+    }
+
+    @Override
+    protected Long getEntityId(Training entity) {
+        return entity.getId();
+    }
+}
