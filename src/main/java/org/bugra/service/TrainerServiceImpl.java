fe81f0c3d64e18dc72d6601d0baae045a3c5d88d
Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-06-28 17:37:42 +0300

    feat: implement trainer service

---
+++ src/main/java/org/bugra/service/TrainerServiceImpl.java
@@ -0,0 +1,69 @@
+package org.bugra.service;
+
+import org.bugra.exception.UserNotFoundException;
+import org.bugra.model.Trainer;
+import org.bugra.persistence.repo.TrainerRepo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TrainerServiceImpl implements TrainerService {
+
+    private static final Logger logger = LoggerFactory.getLogger(TrainerServiceImpl.class);
+    private TrainerRepo trainerRepo;
+    private UserCredentialsService userCredentialsService;
+
+    @Override
+    public Trainer createTrainer(Trainer trainer) {
+        if (trainer == null) {
+            throw new IllegalArgumentException("Trainer cannot be null");
+        }
+
+        // Credentials generation
+        trainer.setPassword(userCredentialsService.generateRandomPassword());
+        trainer.setUsername(userCredentialsService.generateUsername(
+                trainer.getFirstName(),
+                trainer.getLastName(),
+                trainerRepo::existsByUsername
+        ));
+
+        Trainer savedTrainer = trainerRepo.save(trainer);
+        logger.info("Trainer created successfully with ID: {} and username: {}", savedTrainer.getId(), savedTrainer.getUsername());
+        return savedTrainer;
+    }
+
+    @Override
+    public Trainer updateTrainer(Trainer trainer) {
+        if (trainer == null || trainer.getId() == null) {
+            logger.error("Update failed: Trainer or ID is null");
+            throw new IllegalArgumentException("Trainer or Trainer ID cannot be null");
+        }
+
+        Trainer updated = trainerRepo.updateById(trainer)
+                .orElseThrow(() -> new UserNotFoundException("Trainer not found with id: " + trainer.getId()));
+
+        logger.info("Trainer updated successfully with ID: {}", trainer.getId());
+        return updated;
+    }
+
+    @Override
+    public Trainer getTrainer(long trainerId) {
+        return trainerRepo.findById(trainerId)
+                .orElseThrow(() -> {
+                    logger.warn("Trainer with id: {} not found", trainerId);
+                    return new UserNotFoundException("Trainer not found with id: " + trainerId);
+                });
+    }
+
+    @Autowired
+    public void setTrainerRepo(TrainerRepo trainerRepo) {
+        this.trainerRepo = trainerRepo;
+    }
+
+    @Autowired
+    public void setUserCredentialsService(UserCredentialsService userCredentialsService) {
+        this.userCredentialsService = userCredentialsService;
+    }
+}
