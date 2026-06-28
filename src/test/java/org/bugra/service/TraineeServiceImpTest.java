445306d02da27611c779e5af1c344a28df44cbed
Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-06-28 15:09:59 +0300

    test:(trainee service): add unit tests for getTrainee and generateUsername methods

---
+++ src/test/java/org/bugra/service/TraineeServiceImpTest.java
@@ -0,0 +1,114 @@
+package org.bugra.service;
+
+import org.bugra.exception.UserNotFoundException;
+import org.bugra.model.Trainee;
+import org.bugra.persistence.repo.TraineeRepo;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+
+@ExtendWith(MockitoExtension.class)
+class TraineeServiceImpTest {
+
+    @Mock
+    TraineeRepo traineeRepo;
+
+    @InjectMocks
+    TraineeServiceImp traineeService;
+
+    @Test
+    void createTrainee() {
+    }
+
+    @Test
+    void updateTrainee() {
+    }
+
+    @Test
+    void deleteTrainee() {
+    }
+
+    @Test
+    @DisplayName("Should successfully return trainee when trainee exists")
+    void getTrainee_shouldReturnTrainee() {
+
+        //Given
+        long id = 1L;
+        String userName = "john.doe";
+        Trainee mockTrainee = new Trainee();
+        mockTrainee.setId(id);
+        mockTrainee.setUsername(userName);
+
+        // user exists return mock user
+        when(traineeRepo.findById(id)).thenReturn(Optional.of(mockTrainee));
+
+        Trainee result = traineeService.getTrainee(id);
+
+        assertNotNull(result);
+        assertEquals(id, result.getId());
+        assertEquals(userName, result.getUsername());
+
+        verify(traineeRepo, times(1)).findById(id);
+    }
+
+    @Test
+    @DisplayName("Should throw UserNotFoundException when trainee does not exists")
+    void getTrainee_shouldThrowWhenTraineeNotExists(){
+        long id = 1L;
+
+        // user not exists return empty
+        when(traineeRepo.findById(id)).thenReturn(Optional.empty());
+
+        assertThrows(UserNotFoundException.class,
+                () -> traineeService.getTrainee(id));
+
+        verify(traineeRepo, times(1)).findById(id);
+    }
+
+    @Test
+    @DisplayName("Should generate directly username if there is not similarity int storage")
+    void generateUsername_shouldGenerateUsername() {
+        String firstName = "John";
+        String lastName = "Doe";
+        String expectedUserName = "john.doe";
+
+        String actualUsername = traineeService.generateUsername(firstName,lastName);
+
+        assertEquals(expectedUserName, actualUsername);
+    }
+
+    @Test
+    @DisplayName("Should append and increment counter when the generated username is already taken")
+    void generateUsername_shouldHandleDuplicates() {
+        String firstName = "John";
+        String lastName = "Doe";
+        String expectedUserName = "john.doe2";
+
+        // The existing usernames
+        when(traineeRepo.existsByUsername("john.doe")).thenReturn(true);
+        when(traineeRepo.existsByUsername("john.doe1")).thenReturn(true);
+
+
+        String actualUsername = traineeService.generateUsername(firstName,lastName);
+
+
+        assertEquals(expectedUserName, actualUsername);
+
+        verify(traineeRepo, times(1)).existsByUsername("john.doe");
+        verify(traineeRepo, times(1)).existsByUsername("john.doe1");
+        verify(traineeRepo, times(1)).existsByUsername(expectedUserName);
+    }
+
+    @Test
+    void generateRandomPassword() {
+    }
+}
