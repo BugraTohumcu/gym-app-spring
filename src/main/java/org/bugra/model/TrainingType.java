52d7ada9662452f62e9add55e62bf759ccb1ba09
Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-07-03 15:50:28 +0300

    feat: create training type entity

--- src/main/java/org/bugra/model/TrainingType.java
+++ src/main/java/org/bugra/model/TrainingType.java
@@ -1,8 +1,19 @@
 package org.bugra.model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class TrainingType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
     private String trainingTypeName;
 
-    public String getTrainingTypeName() { return trainingTypeName; }
-    public void setTrainingTypeName(String name) { this.trainingTypeName = name; }
 }
