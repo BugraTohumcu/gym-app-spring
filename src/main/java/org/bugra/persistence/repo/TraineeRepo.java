754f32e77ef2c4aa12665af14cf650eef22f91ac
Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-07-04 14:48:12 +0300

    refac: remove storage maps

--- src/main/java/org/bugra/persistence/repo/TraineeRepo.java
+++ src/main/java/org/bugra/persistence/repo/TraineeRepo.java
@@ -1,22 +1,13 @@
 package org.bugra.persistence.repo;
 
-import org.bugra.annotation.StorageQualifier;
-import org.bugra.enums.StorageType;
 import org.bugra.model.Trainee;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.util.Map;
 
 @Repository
-public class TraineeRepo extends UserRepo<Trainee> {
+public class TraineeRepo extends AbstractRepository<Trainee, Long> {
 
     public TraineeRepo() {
-        super();
-    }
-
-    @Autowired
-    public void setStorageMap(@StorageQualifier(StorageType.TRAINEE) Map<Long, Trainee> storageMap) {
-        super.setStorageMap(storageMap);
+        super(Trainee.class, Trainee::getId);
     }
 }
--- src/main/java/org/bugra/persistence/repo/TrainerRepo.java
+++ src/main/java/org/bugra/persistence/repo/TrainerRepo.java
@@ -1,21 +1,13 @@
 package org.bugra.persistence.repo;
 
-import org.bugra.annotation.StorageQualifier;
-import org.bugra.enums.StorageType;
 import org.bugra.model.Trainer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.util.Map;
-
 @Repository
-public class TrainerRepo extends UserRepo<Trainer> {
+public class TrainerRepo extends AbstractRepository<Trainer, Long> {
 
-    public TrainerRepo() { super(); }
-
-
-    @Autowired
-    public void setStorageMap(@StorageQualifier(StorageType.TRAINER) Map<Long, Trainer> storageMap) {
-        super.setStorageMap(storageMap);
+    public TrainerRepo() {
+        super(Trainer.class, Trainer::getId);
     }
+
 }
