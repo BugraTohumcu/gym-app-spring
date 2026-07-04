2c8d241e4f4b6e040e8f3b797f913841eee9d6f8
Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-07-04 21:27:45 +0300

    feat: declare findUsernameStartingWith

--- src/main/java/org/bugra/persistence/repo/UsernameCapable.java
+++ src/main/java/org/bugra/persistence/repo/UsernameCapable.java
@@ -1,5 +1,7 @@
 package org.bugra.persistence.repo;
 
+import java.util.List;
+
 /**
  * Specialized repository interface for entities that possess a username and require sequential ID generation.
  * Separated from the global CrudRepo to respect the Interface Segregation Principle.
@@ -10,4 +12,12 @@ public interface UsernameCapable {
      * Checks if a specific username is already taken by any entity in the storage.
      */
     boolean existsByUsername(String username);
+
+
+    /**
+     * <p>Retrieves all the usernames with the provided base username (eg: "john.doe")</p>
+     * @param basename the base username to retrieve similar usernames
+     * @return the {@link List} of all similar usernames
+     * */
+    List<String> findUsernameStartingWith(String basename);
 }
