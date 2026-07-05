c82c85e84e0f1cca0e64f684311cab966a462cb7
Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-07-05 20:29:45 +0300

    feat: add findTrainerByUsername method

--- src/main/java/org/bugra/persistence/repo/TrainerRepo.java
+++ src/main/java/org/bugra/persistence/repo/TrainerRepo.java
@@ -1,6 +1,12 @@
 package org.bugra.persistence.repo;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.Root;
+import org.bugra.exception.UserNotFoundException;
 import org.bugra.model.Trainer;
+import org.bugra.model.User;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -10,4 +16,22 @@ public class TrainerRepo extends AbstractRepository<Trainer, Long> {
         super(Trainer.class, Trainer::getId);
     }
 
+    public Trainer findTrainerByUsername(String username) {
+        if (username == null || username.isBlank()) {
+            throw new IllegalArgumentException("Username cannot be null or blank");
+        }
+
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Trainer> cq = cb.createQuery(Trainer.class);
+
+        Root<Trainer> root = cq.from(Trainer.class);
+        Join<Trainer, User> userJoin = root.join("user");
+
+        cq.where(cb.equal(userJoin.get("username"), username));
+
+        return entityManager.createQuery(cq)
+                .getResultStream()
+                .findFirst()
+                .orElseThrow(() -> new UserNotFoundException("Trainee not found with username: " + username));
+    }
 }
