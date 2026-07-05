a5aa1205d9177ee74dfa18850684af58bfe9564d
Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-07-05 15:04:46 +0300

    fead: declare changePassword method

--- src/main/java/org/bugra/service/AuthService.java
+++ src/main/java/org/bugra/service/AuthService.java
@@ -1,8 +1,10 @@
 package org.bugra.service;
 
+import org.bugra.dto.ChangePassword;
 import org.bugra.dto.LoginUser;
 import org.bugra.dto.UserResponse;
 import org.bugra.util.UserSession;
+import org.bugra.exception.InvalidPasswordException;
 
 public interface AuthService {
 
@@ -10,6 +12,17 @@ public interface AuthService {
      * Validates provided username and password and sets current {@link UserSession}
      * @param loginUser wrapper that holds username and password information
      * @return {@link UserResponse}
+     * @throws InvalidPasswordException if provided password does not match
      * */
     UserResponse login(LoginUser loginUser);
+
+
+
+    /**
+     * Validates provided password and changes with provided with new password
+     * @param changePassword holds current and new password
+     * @return true if password successfully changed false otherwise
+     * @throws InvalidPasswordException if provided password does not match
+     * */
+    boolean changePassword(ChangePassword changePassword);
 }
