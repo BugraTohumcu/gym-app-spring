b5db51998c2ead2f803914d0e9511a1cbf44b66c
Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-07-05 14:17:33 +0300

    refac: update login method return type User to UserResponse

--- src/main/java/org/bugra/service/AuthService.java
+++ src/main/java/org/bugra/service/AuthService.java
@@ -1,7 +1,7 @@
 package org.bugra.service;
 
 import org.bugra.dto.LoginUser;
-import org.bugra.model.User;
+import org.bugra.dto.UserResponse;
 import org.bugra.util.UserSession;
 
 public interface AuthService {
@@ -9,7 +9,7 @@ public interface AuthService {
     /**
      * Validates provided username and password and sets current {@link UserSession}
      * @param loginUser wrapper that holds username and password information
-     * @return {@link User} object that retrieved from database
+     * @return {@link UserResponse}
      * */
-    User login(LoginUser loginUser);
+    UserResponse login(LoginUser loginUser);
 }
