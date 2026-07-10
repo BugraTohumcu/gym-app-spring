4c4cc9e161a7e77e6f2db6b3f291dd6e745981a4
Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-07-10 12:52:07 +0300

    feat: add training validator

---
+++ src/main/java/org/bugra/util/TrainingValidator.java
@@ -0,0 +1,24 @@
+package org.bugra.util;
+
+import org.bugra.dto.CreateTraining;
+
+import java.time.LocalDate;
+
+public final class TrainingValidator {
+
+    private TrainingValidator() {}
+
+    public static void validate(CreateTraining createTraining) {
+        ValidationUtils.requireNotNull(createTraining, "Training");
+        ValidationUtils.requireNonBlank(createTraining.trainingName(), "Training name");
+        ValidationUtils.requireNonBlank(createTraining.trainingTypeName(), "Training type");
+        ValidationUtils.requireNonBlank(createTraining.trainerUsername(), "Trainer username");
+        ValidationUtils.requireNotNull(createTraining.traineeId(), "Trainee id");
+        validateDateAndDuration(createTraining.trainingDate(), createTraining.trainingDuration());
+    }
+
+    public static void validateDateAndDuration(LocalDate date, int duration) {
+        ValidationUtils.requireNotNull(date, "Training date");
+        ValidationUtils.requireTrue(duration > 0, "Training duration must be positive");
+    }
+}
