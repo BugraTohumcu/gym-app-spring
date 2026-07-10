1140ad34d2507d42dc6ba6be23916b162c3bbe81
Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-07-10 10:34:12 +0300

    test: add findAllByUsernames integration test

--- src/test/java/org/bugra/persistence/repo/TrainerRepoTest.java
+++ src/test/java/org/bugra/persistence/repo/TrainerRepoTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 
@@ -60,5 +61,46 @@ class TrainerRepoTest extends BaseJpaTest {
     @DisplayName("Should throw IllegalArgumentException when username is null")
     void findAllNotAssignedToTrainee_shouldThrowExceptionWhenUsernameIsNull() {
         assertThrows(IllegalArgumentException.class, () -> trainerRepo.findAllNotAssignedToTrainee(null));
+    }
+
+    @Test
+    @DisplayName("Should return empty list when usernames are null")
+    void findAllByUsernames_shouldReturnEmptyWhenUsernameNull(){
+        List<Trainer> result = trainerRepo.findAllByUsernames(null);
+
+        assertEquals(Collections.emptyList(), result);
+    }
+
+    @Test
+    @DisplayName("Should return empty list when username list is empty")
+    void findAllByUsernames_shouldReturnEmptyWhenUsernameEmpty(){
+        List<Trainer> result = trainerRepo.findAllByUsernames(Collections.emptyList());
+
+        assertEquals(Collections.emptyList(), result);
+    }
+
+    @Test
+    @DisplayName("Should return trainer usernames when usernames exists")
+    void findAllByUsernames_shouldReturnUsernames(){
+        List<String> usernames = List.of("john.doe", "jane.smith", "john.smith");
+
+
+        createAndSaveTrainer("john.doe");
+        createAndSaveTrainer("jane.smith");
+        createAndSaveTrainer("john.smith");
+
+        em.flush();
+        em.clear();
+
+        List<Trainer> result = trainerRepo.findAllByUsernames(usernames);
+
+        assertNotNull(result, "Should not be null");
+        assertEquals(3, result.size(), "Should find exactly 3 trainers");
+
+        List<String> resultUsernames = result.stream()
+                .map(trainer -> trainer.getUser().getUsername())
+                .toList();
+
+        assertTrue(resultUsernames.containsAll(usernames), "Should contain all usernames");
     }
 }
