dcc18cd16f051edaa515562d7d4aa2410971bdce
Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-07-18 13:40:53 +0300

    feat: add transactionID filter

--- src/main/java/org/bugra/config/AppInitializer.java
+++ src/main/java/org/bugra/config/AppInitializer.java
@@ -1,26 +1,30 @@
 package org.bugra.config;
 
-import jakarta.servlet.FilterRegistration;
-import jakarta.servlet.ServletContext;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRegistration;
+import jakarta.servlet.*;
+import org.bugra.filter.TransactionFilter;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.DispatcherServlet;
+
+import java.util.EnumSet;
 
 public class AppInitializer implements WebApplicationInitializer {
     @Override
-    public void onStartup(final ServletContext servletContext) throws ServletException {
+    public void onStartup(final ServletContext servletContext) {
 
         AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
         context.register(AppConfig.class);
 
-        // DispatcherServlet'i bu Context ile oluşturup kaydediyoruz
         ServletRegistration.Dynamic dispatcher =
                 servletContext.addServlet("dispatcher", new DispatcherServlet(context));
         dispatcher.setLoadOnStartup(1);
         dispatcher.addMapping("/");
 
+        // add transaction id filter
+        FilterRegistration.Dynamic transactionIdFilter =
+                servletContext.addFilter("transactionIdFilter", new TransactionFilter());
+
+        transactionIdFilter.addMappingForUrlPatterns(
+                EnumSet.of(DispatcherType.REQUEST), false, "/*");
     }
 }
---
+++ src/main/java/org/bugra/filter/TransactionFilter.java
@@ -0,0 +1,30 @@
+package org.bugra.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.MDC;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.UUID;
+
+public class TransactionFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException
+    {
+        try{
+
+            String transactionId = UUID.randomUUID().toString();
+
+            MDC.put("transactionId", transactionId);
+            filterChain.doFilter(request,response);
+        }finally {
+            MDC.clear();
+        }
+    }
+}
