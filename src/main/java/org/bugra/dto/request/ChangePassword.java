eedc7b38f87aa2df11c6f182db199a1b5c3df6dc
Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-07-19 17:55:21 +0300

    feat: add validation

--- src/main/java/org/bugra/dto/request/ChangePassword.java
+++ src/main/java/org/bugra/dto/request/ChangePassword.java
@@ -1,7 +1,22 @@
 package org.bugra.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.bugra.shared.ValidationMessages;
+
 public record ChangePassword(
+
+
+        @NotBlank(message = ValidationMessages.USERNAME_REQUIRED)
+        @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
+        String username,
+
+        @NotBlank(message = ValidationMessages.PASSWORD_REQUIRED)
+        @Size(min = 3, max = 20, message = "Password must be between 3 and 20 characters")
         String currentPassword,
+
+        @NotBlank(message = ValidationMessages.PASSWORD_REQUIRED)
+        @Size(min = 3, max = 20, message = "Password must be between 3 and 20 characters")
         String newPassword
 ) {
 }
