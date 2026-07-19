be87e7437c5d2ca1b8935d8335865ea0d7bc3508
Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-07-19 18:13:27 +0300

    feat: add change password

--- src/main/java/org/bugra/controller/AuthController.java
+++ src/main/java/org/bugra/controller/AuthController.java
@@ -1,12 +1,14 @@
 package org.bugra.controller;
 
 import jakarta.validation.Valid;
+import org.bugra.dto.request.ChangePassword;
 import org.bugra.dto.request.LoginUser;
 import org.bugra.dto.response.UserResponse;
 import org.bugra.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,6 +31,21 @@ public class AuthController {
         return ResponseEntity.ok()
                 .header("Authorization", token)
                 .body(userResponse);
+    }
+
+    @PutMapping("/login")
+    public ResponseEntity<Void> changePassword(
+            @Valid @RequestBody ChangePassword changePassword
+            )
+    {
+       authService.changePassword(changePassword);
+        String token = Base64.getEncoder().encodeToString(
+                (changePassword.username() + ":" + changePassword.newPassword()).getBytes()
+        );
+
+        return ResponseEntity.ok()
+                .header("Authorization", token)
+                .build();
     }
 
 
