0cbb109df0059d4209617c86674f5541fcc304f9
Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-07-19 13:05:29 +0300

    feat: add trainee profile respose dto

---
+++ src/main/java/org/bugra/dto/response/TraineeProfileResponse.java
@@ -0,0 +1,27 @@
+package org.bugra.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Builder
+public record TraineeProfileResponse(
+        String firstName,
+        String lastName,
+
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+        LocalDate dateOfBirth,
+
+        String address,
+        boolean isActive,
+
+        List<TrainerSummary> trainers
+) {
+    public record TrainerSummary(
+            String firstName,
+            String lastName,
+            String specialization
+    ) {}
+}
