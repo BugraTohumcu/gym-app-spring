25fe92d4fcd0690ea3657794cac2d7965c718c1d
Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-07-19 21:53:21 +0300

    feat: add update trainers list dto

---
+++ src/main/java/org/bugra/dto/request/UpdateTrainersList.java
@@ -0,0 +1,17 @@
+package org.bugra.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.bugra.shared.ValidationMessages;
+
+import java.util.List;
+
+public record UpdateTrainersList(
+        @NotBlank(message = ValidationMessages.USERNAME_REQUIRED)
+        @Size(min = 3, max = 50, message = ValidationMessages.USERNAME_SIZE)
+        String username,
+
+        @NotBlank(message = ValidationMessages.TRAINER_LIST_SIZE)
+        List<String> trainerUsernames
+) {
+}
