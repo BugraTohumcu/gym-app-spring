f8d7bd6a2165640e7721bd1e0d134f8122b766af
Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-07-19 17:39:12 +0300

    refac: remove unused packages

--- src/main/java/org/bugra/filter/AuthFilter.java
+++ src/main/java/org/bugra/filter/AuthFilter.java
@@ -6,7 +6,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.bugra.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 import java.util.Base64;
