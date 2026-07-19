a3fedf85da2eee0364f3be8a0bffef36b6d0de40
Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-07-19 21:42:53 +0300

    feat: add trainee summary mapper method

--- src/main/java/org/bugra/mapper/TraineeResponseMapper.java
+++ src/main/java/org/bugra/mapper/TraineeResponseMapper.java
@@ -2,7 +2,10 @@ package org.bugra.mapper;
 
 import org.bugra.dto.response.TraineeProfileResponse;
 import org.bugra.model.Trainee;
+import org.bugra.model.Trainer;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class TraineeResponseMapper {
@@ -25,5 +28,16 @@ public class TraineeResponseMapper {
                         ))
                         .toList())
                 .build();
+    }
+
+    public List<TraineeProfileResponse.TrainerSummary> mapToTrainerSummary(List<Trainer> trainers){
+        return trainers.stream()
+                .map(trainer -> new TraineeProfileResponse.TrainerSummary(
+                        trainer.getUser().getUsername(),
+                        trainer.getUser().getFirstName(),
+                        trainer.getUser().getLastName(),
+                        trainer.getSpecialization().getTrainingTypeName()
+                ))
+                .toList();
     }
  }
