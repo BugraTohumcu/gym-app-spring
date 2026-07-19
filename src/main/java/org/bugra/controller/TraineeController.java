9be50768ded9073b382d0d76779b270b0a11c8e0
Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-07-19 13:52:52 +0300

    feat: add get by username

--- src/main/java/org/bugra/controller/TraineeController.java
+++ src/main/java/org/bugra/controller/TraineeController.java
@@ -3,20 +3,20 @@ package org.bugra.controller;
 
 import jakarta.validation.Valid;
 import org.bugra.dto.request.RegisterTrainee;
+import org.bugra.dto.response.TraineeProfileResponse;
 import org.bugra.dto.response.UserResponse;
+import org.bugra.mapper.TraineeResponseMapper;
+import org.bugra.model.Trainee;
 import org.bugra.service.TraineeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "/trainee", consumes = {"application/json"}, produces = {"application/json"})
+@RequestMapping(value = "/trainee")
 public class TraineeController {
 
     private static final Logger logger = LoggerFactory.getLogger(TraineeController.class);
@@ -29,6 +29,15 @@ public class TraineeController {
     {
             UserResponse response = traineeService.createTrainee(registerTrainee);
             return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/{username}")
+    public ResponseEntity<TraineeProfileResponse> getTraineeProfile(
+            @PathVariable(value = "username") String username
+    ){
+        Trainee trainee = traineeService.getTraineeByUsername(username);
+        TraineeProfileResponse response = TraineeResponseMapper.mapToTraineeProfileResponse(trainee);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @Autowired
