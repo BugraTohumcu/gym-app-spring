efe86312b8c6cec21881099fee9d32d43c443268
Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-07-19 16:20:55 +0300

    feat: add validation for LoginUser

--- src/main/java/org/bugra/dto/request/LoginUser.java
+++ src/main/java/org/bugra/dto/request/LoginUser.java
@@ -1,3 +1,15 @@
 package org.bugra.dto.request;
 
-public record LoginUser(String username,  String password) { }
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.bugra.shared.ValidationMessages;
+
+public record LoginUser(
+
+        @NotBlank(message = ValidationMessages.USERNAME_REQUIRED)
+        @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
+        String username,
+
+        @NotBlank(message = ValidationMessages.PASSWORD_REQUIRED)
+        @Size(min = 3, max = 20, message = "Password must be between 3 and 20 characters")
+        String password) { }
