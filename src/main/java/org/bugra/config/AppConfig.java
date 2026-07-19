b5355b7152bdc58a00703221a3e500baee468ebf
Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-07-19 17:12:45 +0300

    feat: add auth filter

--- src/main/java/org/bugra/config/AppConfig.java
+++ src/main/java/org/bugra/config/AppConfig.java
@@ -1,12 +1,22 @@
 package org.bugra.config;
 
 
+import org.bugra.filter.AuthFilter;
+import org.bugra.service.AuthService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@ComponentScan(basePackages = {"org.bugra", "org.springdoc"})
+@ComponentScan(basePackages = {"org.bugra"})
 @Import({ StorageConfig.class, WebConfig.class})
 public class AppConfig {
+
+    @Bean
+    public AuthFilter authFilter(AuthService authService){
+        AuthFilter authFilter = new AuthFilter();
+        authFilter.setAuthService(authService);
+        return authFilter;
+    }
 }
--- src/main/java/org/bugra/config/AppInitializer.java
+++ src/main/java/org/bugra/config/AppInitializer.java
@@ -1,10 +1,12 @@
 package org.bugra.config;
 
 import jakarta.servlet.*;
+import org.bugra.filter.AuthFilter;
 import org.bugra.filter.RestLoggingFilter;
 import org.bugra.filter.TransactionFilter;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.DelegatingFilterProxy;
 import org.springframework.web.servlet.DispatcherServlet;
 
 import java.util.EnumSet;
@@ -15,6 +17,8 @@ public class AppInitializer implements WebApplicationInitializer {
 
         AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
         context.register(AppConfig.class);
+        context.setServletContext(servletContext);
+        context.refresh();
 
         ServletRegistration.Dynamic dispatcher =
                 servletContext.addServlet("dispatcher", new DispatcherServlet(context));
@@ -28,11 +32,18 @@ public class AppInitializer implements WebApplicationInitializer {
         transactionIdFilter.addMappingForUrlPatterns(
                 EnumSet.of(DispatcherType.REQUEST), false, "/*");
 
-        // add rest logging filter to end
+        // filter for logging for incoming request
         FilterRegistration.Dynamic restLoggingFilter =
                 servletContext.addFilter("restLoggingFilter", new RestLoggingFilter());
 
         restLoggingFilter.addMappingForUrlPatterns(
+                EnumSet.of(DispatcherType.REQUEST),true, "/*");
+
+        // filter for auth validation
+        FilterRegistration.Dynamic authFilter =
+                servletContext.addFilter("authFilter", new DelegatingFilterProxy("authFilter"));
+
+        authFilter.addMappingForUrlPatterns(
                 EnumSet.of(DispatcherType.REQUEST),true, "/*");
     }
 }
