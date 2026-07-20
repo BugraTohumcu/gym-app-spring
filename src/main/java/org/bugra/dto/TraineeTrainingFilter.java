7962cc984fe387662f6ca2b8685edf55548bfa98
Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-07-20 13:12:58 +0300

    feat: add date validation for fromDate to endDate

--- src/main/java/org/bugra/dto/TraineeTrainingFilter.java
+++ src/main/java/org/bugra/dto/TraineeTrainingFilter.java
@@ -1,11 +1,24 @@
 package org.bugra.dto;
 
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.bugra.shared.ValidationMessages;
+
 import java.time.LocalDate;
 
 public record TraineeTrainingFilter(
+
+        @NotBlank(message = ValidationMessages.USERNAME_REQUIRED)
+        @Size(min = 3, max = 50, message = ValidationMessages.USERNAME_SIZE)
         String traineeUsername,
         LocalDate fromDate,
         LocalDate toDate,
         String trainerName,
         String trainingType
-) {}
+) {
+    @AssertTrue(message = ValidationMessages.DATE_INTERVAL)
+    public boolean isDateRangeValid() {
+        return fromDate == null || toDate == null || !fromDate.isAfter(toDate);
+    }
+}
