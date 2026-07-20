8b24215e9a75663b544fcb5c6595b5a9f93880bb
Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-07-20 13:12:01 +0300

    feat: add training response mapper

---
+++ src/main/java/org/bugra/mapper/TrainingResponseMapper.java
@@ -0,0 +1,23 @@
+package org.bugra.mapper;
+
+import org.bugra.dto.response.TraineeTrainings;
+import org.bugra.model.Training;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class TrainingResponseMapper {
+
+    public List<TraineeTrainings> mapToTraineeTrainings(List<Training> trainings){
+        return trainings.stream()
+                .map(training -> TraineeTrainings.builder()
+                        .trainingName(training.getTrainingName())
+                        .trainingType(training.getTrainingType())
+                        .date(training.getTrainingDate())
+                        .duration(training.getTrainingDuration())
+                        .trainerName(training.getTrainer().getUser().getUsername())
+                        .build())
+                .toList();
+    }
+}
