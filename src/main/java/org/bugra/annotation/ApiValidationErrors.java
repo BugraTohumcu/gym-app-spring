995f8a7c39592e083ea9c2d49eda978e0c1c9bbe
Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-07-21 15:31:04 +0300

    refac: use custom api response annotation

--- src/main/java/org/bugra/annotation/ApiValidationErrors.java
+++ src/main/java/org/bugra/annotation/ApiValidationErrors.java
@@ -1,11 +1,5 @@
 package org.bugra.annotation;
 
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import org.bugra.dto.response.ErrorResponse;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -13,11 +7,9 @@ import java.lang.annotation.Target;
 
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
-@ApiResponses({
-        @ApiResponse(responseCode = "400", description = "Invalid JSON payload or formatting issue",
-                content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
-        @ApiResponse(responseCode = "422", description = "Validation failed for request fields",
-                content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+@ApiErrorResponses({
+        @ApiErrorResponse(responseCode = "400", description = "Invalid JSON payload or formatting issue"),
+        @ApiErrorResponse(responseCode = "422", description = "Validation failed for request fields")
 })
 public @interface ApiValidationErrors {
 }
