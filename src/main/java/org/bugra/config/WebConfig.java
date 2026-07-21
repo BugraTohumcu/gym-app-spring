4a6532687b9ad9ec22c3fa29d8885279acadefee
Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-07-21 14:46:13 +0300

    feat: add swagger config

--- src/main/java/org/bugra/config/WebConfig.java
+++ src/main/java/org/bugra/config/WebConfig.java
@@ -1,35 +1,57 @@
 package org.bugra.config;
 
-
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
 @EnableWebMvc
+@ComponentScan(basePackages = {"org.bugra", "org.springdoc"})
 public class WebConfig implements WebMvcConfigurer {
 
-
     @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(0, new ByteArrayHttpMessageConverter());
+
+        converters.add(1, new StringHttpMessageConverter());
+
         ObjectMapper mapper = new ObjectMapper();
-
         mapper.registerModule(new JavaTimeModule());
-
-        // accept exact type
         mapper.configure(MapperFeature.ALLOW_COERCION_OF_SCALARS, false);
-
-        // return dates as a string
         mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 
-        converters.add(new MappingJackson2HttpMessageConverter(mapper));
+        converters.add(2, new MappingJackson2HttpMessageConverter(mapper));
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("GymApp rest api documentation"));
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/swagger-ui/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/swagger-ui/");
+
+        registry.addResourceHandler("/v3/api-docs/**")
+                .addResourceLocations("classpath:/META-INF/resources/");
     }
 }
