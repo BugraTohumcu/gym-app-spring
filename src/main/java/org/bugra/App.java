051c4232f80ff823de7307738faf95bbf657e66e
Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-07-24 15:19:11 +0300

    feat: add spring boot run

--- src/main/java/org/bugra/App.java
+++ src/main/java/org/bugra/App.java
@@ -1,31 +1,11 @@
 package org.bugra;
 
-import org.apache.catalina.LifecycleException;
-import org.apache.catalina.startup.Tomcat;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.File;
-
+@SpringBootApplication
 public class App {
-    private static final Logger logger = LoggerFactory.getLogger(App.class);
-    public static void main(String[] args) throws LifecycleException {
-
-        Tomcat tomcat = new Tomcat();
-        tomcat.setPort(8080);
-        tomcat.setHostname("localhost");
-        tomcat.getConnector();
-
-        File webappDir = new File("src/main/webapp");
-        if (!webappDir.exists()) {
-            webappDir.mkdirs();
-        }
-
-        tomcat.addWebapp("", webappDir.getAbsolutePath());
-
-
-        tomcat.start();
-        logger.warn("The server is running at: http://localhost:8080/");
-        tomcat.getServer().await();
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
     }
 }
