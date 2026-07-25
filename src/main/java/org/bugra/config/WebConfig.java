828e779aee768461d450ba021971d2627eae2dfe
Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-07-25 16:10:38 +0300

    refac: remove message converter config

--- src/main/java/org/bugra/config/WebConfig.java
+++ src/main/java/org/bugra/config/WebConfig.java
@@ -10,34 +10,13 @@ import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.ByteArrayHttpMessageConverter;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.List;
 
 @Configuration
-@EnableWebMvc
 @ComponentScan(basePackages = {"org.bugra", "org.springdoc"})
 public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.add(0, new ByteArrayHttpMessageConverter());
-
-        converters.add(1, new StringHttpMessageConverter());
-
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JavaTimeModule());
-        mapper.configure(MapperFeature.ALLOW_COERCION_OF_SCALARS, false);
-        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-
-        converters.add(2, new MappingJackson2HttpMessageConverter(mapper));
-    }
 
     @Bean
     public OpenAPI customOpenAPI() {
