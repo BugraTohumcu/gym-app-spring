80d641cfc80f6286dc0fc589e29aed3f5110df19
Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-07-26 09:33:23 +0300

    test: add unit tests for up and down status

---
+++ src/test/java/org/bugra/monitor/health/TrainingTypeRepoIndicatorTest.java
@@ -0,0 +1,53 @@
+package org.bugra.monitor.health;
+
+import org.bugra.model.TrainingType;
+import org.bugra.persistence.repo.TrainingTypeRepo;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class TrainingTypeRepoIndicatorTest {
+
+    @Mock
+    private TrainingTypeRepo trainingTypeRepo;
+
+    @InjectMocks
+    private TrainingTypeRepoIndicator trainingTypeRepoIndicator;
+
+    @Test
+    @DisplayName("Should status be UP when training type table is non-empty")
+    void health_shouldBeUpWhenTableIsNonEmpty(){
+        when(trainingTypeRepo.findAllTypes()).thenReturn(List.of(new TrainingType()));
+
+        Health health = trainingTypeRepoIndicator.health();
+        assertEquals("UP", health.getStatus().toString());
+        assertEquals("non-empty", health.getDetails().get("trainingTypeTable").toString());
+    }
+
+    @Test
+    @DisplayName("Should status be DOWN when training type table is empty")
+    void health_shouldBeDOWNWhenTableIsEmpty(){
+        when(trainingTypeRepo.findAllTypes()).thenReturn(List.of());
+
+        Health health = trainingTypeRepoIndicator.health();
+        assertEquals("DOWN", health.getStatus().toString());
+        assertEquals("empty", health.getDetails().get("trainingTypeTable").toString());
+    }
+}
