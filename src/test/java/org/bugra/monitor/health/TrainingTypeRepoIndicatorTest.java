3ce5609214f62c597e7dc54181f83e1a22fb14d1
Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-07-26 09:34:33 +0300

    feat: remove unused imports

--- src/test/java/org/bugra/monitor/health/TrainingTypeRepoIndicatorTest.java
+++ src/test/java/org/bugra/monitor/health/TrainingTypeRepoIndicatorTest.java
@@ -8,17 +8,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.health.Health;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
+
 
 import java.util.List;
 
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-
 
 import static org.junit.jupiter.api.Assertions.*;
 
