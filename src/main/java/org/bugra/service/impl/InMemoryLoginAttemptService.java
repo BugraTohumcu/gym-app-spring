9418eb8dd2d097091a52367b76d0fdc54d47d316
Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-08-02 21:02:46 +0300

    feat: add block cache

--- src/main/java/org/bugra/service/impl/InMemoryLoginAttemptService.java
+++ src/main/java/org/bugra/service/impl/InMemoryLoginAttemptService.java
@@ -3,29 +3,49 @@ package org.bugra.service.impl;
 import org.bugra.service.LoginAttemptService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 public class InMemoryLoginAttemptService implements LoginAttemptService {
 
     private final int MAX_ATTEMPT = 3;
+    private final int BLOCK_DURATION = 5;
     private final ConcurrentHashMap<String, Integer> attemptCache;
+    private final ConcurrentHashMap<String, LocalDateTime> blockCache;
 
     public InMemoryLoginAttemptService(){
         attemptCache = new ConcurrentHashMap<>();
+        blockCache = new ConcurrentHashMap<>();
     }
     @Override
     public void loginSucceed(String key) {
         attemptCache.remove(key);
+        blockCache.remove(key);
     }
 
     @Override
     public void loginFailed(String key) {
-        attemptCache.merge(key, 1, Integer::sum);
+        int attempt = attemptCache.merge(key, 1, Integer::sum);
+        if(attempt >= MAX_ATTEMPT){
+            blockCache.put(key, LocalDateTime.now().plusMinutes(BLOCK_DURATION));
+        }
     }
 
     @Override
     public boolean isBlocked(String key) {
-        return attemptCache.getOrDefault(key, 0) >= MAX_ATTEMPT;
+        LocalDateTime unBlockTime = blockCache.get(key);
+
+        if(unBlockTime != null){
+            if(LocalDateTime.now().isBefore(unBlockTime)){
+                return true;
+            }else{
+                attemptCache.remove(key);
+                blockCache.remove(key);
+                return false;
+            }
+        }
+
+        return false;
     }
 }
