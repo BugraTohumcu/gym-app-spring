84cd140ba8516c9ca949ef3c5f4cf8dd0530ff29
Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-08-02 21:08:42 +0300

    feat: throw LockedException if user is blocked

--- src/main/java/org/bugra/service/impl/UserDetailsServiceImp.java
+++ src/main/java/org/bugra/service/impl/UserDetailsServiceImp.java
@@ -5,6 +5,8 @@ import org.bugra.exception.UserNotFoundException;
 import org.bugra.model.User;
 import org.bugra.persistence.repo.UserRepo;
 import org.bugra.security.UserPrincipal;
+import org.bugra.service.LoginAttemptService;
+import org.springframework.security.authentication.LockedException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -13,13 +15,18 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class UserDetailsServiceImp implements UserDetailsService {
+
     private final UserRepo userRepo;
+    private final LoginAttemptService loginAttemptService;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         try{
-            User user = userRepo.findByUsername(username);
-            return new UserPrincipal(user);
+            if(loginAttemptService.isBlocked(username)){
+                throw new LockedException("Too many attempts! Please wait for five minutes");
+            }
+                User user = userRepo.findByUsername(username);
+                return new UserPrincipal(user);
         }catch(UserNotFoundException ex){
             throw new UsernameNotFoundException(ex.getMessage());
         }
