d29e09f08ab1d7c16e458f55dffa797bcd791837
Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-08-02 19:47:23 +0300

    feat: configure, password encoder, filter chain, dao provider

--- src/main/java/org/bugra/config/AppConfig.java
+++ src/main/java/org/bugra/config/AppConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Import;
 
 @Configuration
 @ComponentScan(basePackages = {"org.bugra"})
-@Import({ StorageConfig.class, WebConfig.class, FilterConfig.class})
+@Import({ StorageConfig.class, WebConfig.class, SecurityConfig.class})
 public class AppConfig {
 
     @Bean
