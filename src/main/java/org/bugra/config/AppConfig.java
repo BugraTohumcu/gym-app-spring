c940fbc6cedbb9a93f1ca84d9d30bf27afa55c5d
Ahmet Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-08-03 14:17:51 +0300

    refac: remove auth filter bean

--- src/main/java/org/bugra/config/AppConfig.java
+++ src/main/java/org/bugra/config/AppConfig.java
@@ -2,21 +2,16 @@ package org.bugra.config;
 
 
 import org.bugra.filter.AuthFilter;
+import org.bugra.security.JwtTokenProvider;
 import org.bugra.service.AuthService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 @ComponentScan(basePackages = {"org.bugra"})
 @Import({ StorageConfig.class, WebConfig.class, SecurityConfig.class})
 public class AppConfig {
-
-    @Bean
-    public AuthFilter authFilter(AuthService authService){
-        AuthFilter authFilter = new AuthFilter();
-        authFilter.setAuthService(authService);
-        return authFilter;
-    }
 }
