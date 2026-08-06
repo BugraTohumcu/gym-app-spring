2104991733ea5e9477a83764a7ac2db85a0cc2a6
Ahmet Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-08-06 16:00:43 +0300

    feat: add find by username with eager workload fetch

--- workload-service/src/main/java/com/bugra/workloadservice/repo/TrainerRepo.java
+++ workload-service/src/main/java/com/bugra/workloadservice/repo/TrainerRepo.java
@@ -1,7 +1,13 @@
 package com.bugra.workloadservice.repo;
 
 import com.bugra.workloadservice.model.Trainer;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TrainerRepo extends JpaRepository<Trainer, Long> {
+
+    @EntityGraph(attributePaths = {"workloads", "workloads.months"})
+    Optional<Trainer> findByUsername(String username);
 }
