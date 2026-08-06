f1956bd02c0606c65957b56ecda8a8d1fdb0796a
Ahmet Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-08-06 14:12:44 +0300

    refac: switched to monorepo for microservices

--- gym-app-main/src/main/java/org/bugra/config/AppConfig.java
+++ gym-app-main/src/main/java/org/bugra/config/AppConfig.java
@@ -1,14 +1,10 @@
 package org.bugra.config;
 
 
-import org.bugra.filter.AuthFilter;
-import org.bugra.security.JwtTokenProvider;
-import org.bugra.service.AuthService;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.security.core.userdetails.UserDetailsService;
+
 
 @Configuration
 @ComponentScan(basePackages = {"org.bugra"})
---
+++ workload-service/src/test/java/com/bugra/workloadservice/WorkloadServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bugra.workloadservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkloadServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
