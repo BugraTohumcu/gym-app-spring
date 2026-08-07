8f0ec2f6b2295f93ca104e79cbe7a6641f299a9c
Ahmet Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-08-07 16:09:59 +0300

    refac: change trainingDate type to LocalDate

--- gym-app-main/src/main/java/org/bugra/dto/client/SaveTrainerWorkload.java
+++ gym-app-main/src/main/java/org/bugra/dto/client/SaveTrainerWorkload.java
@@ -1,14 +1,16 @@
 package org.bugra.dto.client;
 
+import lombok.Builder;
 import org.bugra.enums.ActionType;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
+@Builder
 public record SaveTrainerWorkload(
         String firstName,
         String lastName,
         String username,
-        LocalDateTime trainingDate,
+        LocalDate trainingDate,
         ActionType actionType,
         boolean isActive,
         int duration
