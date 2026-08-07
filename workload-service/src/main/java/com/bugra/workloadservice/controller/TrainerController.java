acab2b07bcf870f0ff70d92e1b163e2118ce5a2c
Ahmet Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-08-07 14:22:23 +0300

    feat: add getWorkload endpoint

--- workload-service/src/main/java/com/bugra/workloadservice/controller/TrainerController.java
+++ workload-service/src/main/java/com/bugra/workloadservice/controller/TrainerController.java
@@ -2,15 +2,16 @@ package com.bugra.workloadservice.controller;
 
 
 import com.bugra.workloadservice.dto.request.TrainerDto;
+import com.bugra.workloadservice.dto.response.ErrorResponse;
+import com.bugra.workloadservice.dto.response.TrainerWorkloadResponse;
 import com.bugra.workloadservice.service.TrainerService;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/trainer")
@@ -20,7 +21,8 @@ public class TrainerController {
     private final TrainerService trainerService;
 
     @ApiResponse(responseCode = "200", description = "Create new trainer workload record")
-    @ApiResponse(responseCode = "422", description = "Invalid request input")
+    @ApiResponse(responseCode = "422", description = "Invalid request input",
+    content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     @PostMapping
     public ResponseEntity<Void> createTrainerRecord
             (
@@ -31,4 +33,14 @@ public class TrainerController {
         return ResponseEntity.ok().build();
     }
 
+    @ApiResponse(responseCode = "200", description = "Get trainer workload")
+    @GetMapping("/workload")
+    public ResponseEntity<TrainerWorkloadResponse> getWorkload
+            (
+                    @RequestParam("username") String username
+            )
+    {
+        TrainerWorkloadResponse workloads = trainerService.getTrainerWorkload(username);
+        return ResponseEntity.ok().body(workloads);
+    }
 }
