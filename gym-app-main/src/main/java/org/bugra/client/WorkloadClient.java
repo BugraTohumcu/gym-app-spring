eb3b0328f0815ff019ae2d724e691b9a0f352984
Ahmet Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-08-07 16:11:38 +0300

    feat: add workload service feign client config

--- gym-app-main/src/main/java/org/bugra/client/WorkloadClient.java
+++ gym-app-main/src/main/java/org/bugra/client/WorkloadClient.java
@@ -1,11 +1,13 @@
 package org.bugra.client;
 
+import org.bugra.config.WorkloadServiceFeignConfig;
+import org.bugra.dto.client.SaveTrainerWorkload;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(name = "workload-service")
+@FeignClient(name = "workload-service", configuration = WorkloadServiceFeignConfig.class)
 public interface WorkloadClient {
 
     @PostMapping("/workload")
-    void saveTrainerWorkload();
+    void saveTrainerWorkload(SaveTrainerWorkload saveTrainerWorkload);
 }
