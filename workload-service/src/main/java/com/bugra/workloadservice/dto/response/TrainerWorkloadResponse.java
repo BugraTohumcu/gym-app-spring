2ae2c9338fd9f020574f23384d23f29acddf9e4f
Ahmet Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-08-07 12:13:48 +0300

    refac: return map for workload summary

--- workload-service/src/main/java/com/bugra/workloadservice/dto/response/TrainerWorkloadResponse.java
+++ workload-service/src/main/java/com/bugra/workloadservice/dto/response/TrainerWorkloadResponse.java
@@ -1,14 +1,17 @@
 package com.bugra.workloadservice.dto.response;
 
 import com.bugra.workloadservice.model.YearlyWorkload;
+import lombok.Builder;
 
-import java.util.List;
+import java.util.Map;
 
+@Builder
 public record TrainerWorkloadResponse(
         String username,
         String firstName,
         String lastName,
         boolean isActive,
-        List<YearlyWorkload> workloads
+        // Year -> Month, Duration
+        Map<String, Map<String, Integer>> workloads
 ) {
 }
