3d99d3e621a101b468c2a1f41feac5e03aca64a8
Ahmet Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-08-07 12:13:13 +0300

    feat: declare getTrainerWorkload

--- workload-service/src/main/java/com/bugra/workloadservice/service/TrainerService.java
+++ workload-service/src/main/java/com/bugra/workloadservice/service/TrainerService.java
@@ -1,14 +1,23 @@
 package com.bugra.workloadservice.service;
 
 import com.bugra.workloadservice.dto.request.TrainerDto;
+import com.bugra.workloadservice.dto.response.TrainerWorkloadResponse;
 
 public interface TrainerService {
 
 
 
     /**
-     * saves provided trainer
+     * Saves provided trainer
      * @param trainerDto the provided trainer dto to separate into trainer and workload models
      * */
     void saveTrainerRecord(TrainerDto trainerDto);
+
+
+    /**
+     * Retrieves all workload of the trainer
+     * @param username trainer username
+     * @return {@link TrainerWorkloadResponse} dto
+     */
+    TrainerWorkloadResponse getTrainerWorkload(String username);
 }
