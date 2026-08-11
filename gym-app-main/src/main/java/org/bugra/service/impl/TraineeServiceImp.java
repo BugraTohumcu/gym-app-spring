a9a76d23f50507820316af4ae66527cdf095f551
Ahmet Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-08-11 15:03:17 +0300

    refac: remove class level transaction

--- gym-app-main/src/main/java/org/bugra/service/impl/TraineeServiceImp.java
+++ gym-app-main/src/main/java/org/bugra/service/impl/TraineeServiceImp.java
@@ -31,7 +31,6 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-@Transactional
 @Service
 @RequiredArgsConstructor
 public class TraineeServiceImp implements TraineeService {
