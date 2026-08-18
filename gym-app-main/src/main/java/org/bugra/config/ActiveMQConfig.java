c8a97e595c9b925ddf2598767a06d9caabed5a1b
Ahmet Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-08-18 09:40:58 +0300

    feat: add activemq config

---
+++ gym-app-main/src/main/java/org/bugra/config/ActiveMQConfig.java
@@ -0,0 +1,32 @@
+package org.bugra.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class ActiveMQConfig {
+
+    @Bean
+    public MessageConverter messageConverter(ObjectMapper objectMapper){
+        MappingJackson2MessageConverter converter =
+                new MappingJackson2MessageConverter();
+
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+
+        Map<String, Class<?>> typeIdMappings = new HashMap<>();
+        typeIdMappings.put("SaveTrainerWorkload", org.bugra.dto.client.SaveTrainerWorkload.class);
+
+        converter.setTypeIdMappings(typeIdMappings);
+        converter.setObjectMapper(objectMapper);
+
+        return converter;
+    }
+}
---
+++ workload-service/src/main/java/com/bugra/workloadservice/config/ActiveMQConfig.java
@@ -0,0 +1,52 @@
+package com.bugra.workloadservice.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.jms.ConnectionFactory;
+import lombok.Value;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class ActiveMQConfig {
+
+    @Bean
+    public JmsListenerContainerFactory<?> jmsListenerContainerFactory
+            (
+                    ConnectionFactory connectionFactory,
+                    DefaultJmsListenerContainerFactoryConfigurer configurer
+            )
+    {
+        DefaultJmsListenerContainerFactory factory =
+                new DefaultJmsListenerContainerFactory();
+        configurer.configure(factory, connectionFactory);
+
+        return factory;
+    }
+
+    @Bean
+    public MessageConverter messageConverter(ObjectMapper objectMapper){
+        MappingJackson2MessageConverter converter =
+                new MappingJackson2MessageConverter();
+
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+
+        Map<String, Class<?>> typeIdMappings = new HashMap<>();
+        typeIdMappings.put("SaveTrainingWorkload", com.bugra.workloadservice.dto.request.TrainerDto.class);
+
+        converter.setTypeIdMappings(typeIdMappings);
+        converter.setObjectMapper(objectMapper);
+
+        return converter;
+    }
+}
+
