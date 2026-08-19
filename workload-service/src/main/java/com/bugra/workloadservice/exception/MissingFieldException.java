590a24942b374ea30fe80262ad06adf54c564114
Ahmet Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-08-19 10:16:03 +0300

    refac: rename to InvalidFieldException

--- workload-service/src/main/java/com/bugra/workloadservice/exception/MissingFieldException.java
+++
@@ -1,7 +0,0 @@
-package com.bugra.workloadservice.exception;
-
-public class MissingFieldException extends RuntimeException {
-    public MissingFieldException(String message) {
-        super(message);
-    }
-}
