e7b150adaf313924ea9ce17d6db8759f71943019
Ahmet Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-08-20 09:38:23 +0300

    refac: remove unused imports

--- workload-service/src/main/java/com/bugra/workloadservice/config/WebConfig.java
+++ workload-service/src/main/java/com/bugra/workloadservice/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.bugra.workloadservice.config;
 
-
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
@@ -8,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class WebConfig {
-
 
     @Bean
     public OpenAPI openAPI(){
