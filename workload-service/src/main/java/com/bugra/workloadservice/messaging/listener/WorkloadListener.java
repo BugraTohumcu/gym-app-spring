a1059c430058df0a2294e743db7e0c7461eedb26
Ahmet Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-08-20 14:33:35 +0300

    refac: remove conversion and validation check use custom JmsRouter

--- workload-service/src/main/java/com/bugra/workloadservice/messaging/listener/WorkloadListener.java
+++ workload-service/src/main/java/com/bugra/workloadservice/messaging/listener/WorkloadListener.java
@@ -1,55 +1,28 @@
 package com.bugra.workloadservice.messaging.listener;
 
 import com.bugra.workloadservice.dto.request.TrainerDto;
-import com.bugra.workloadservice.messaging.validator.JmsMessageValidator;
+import com.bugra.workloadservice.messaging.router.JmsMessageRouter;
 import com.bugra.workloadservice.service.TrainerService;
-import jakarta.jms.JMSException;
 import jakarta.jms.Message;
-import jakarta.validation.ConstraintViolation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.annotation.JmsListener;
-import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.support.converter.MessageConversionException;
-import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.stereotype.Component;
-
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class WorkloadListener {
 
-    private final MessageConverter messageConverter;
     private final TrainerService trainerService;
-    private final JmsTemplate jmsTemplate;
-    private final JmsMessageValidator validator;
+    private final JmsMessageRouter router;
+
+    @Value("${app.activemq.queues.workload-dlq}")
+    private String workloadDlq;
 
     @JmsListener(destination = "${app.activemq.queues.workload}", containerFactory = "jmsListenerContainerFactory")
-    public void onWorkloadMessageReceived(Message message) throws JMSException {
-        TrainerDto trainerDto;
-
-        // check message conversion errors
-        try {
-            trainerDto = (TrainerDto) messageConverter.fromMessage(message);
-        }catch (MessageConversionException ex){
-            log.error("Message could not be parsed: {}", ex.getMessage());
-            jmsTemplate.send("workload-dlq", session -> message);
-            return;
-        }
-
-        // check validation errors, if there are send to the dlq
-        Set<ConstraintViolation<TrainerDto>> violations = validator.validate(trainerDto);
-        if(!violations.isEmpty()){
-            String reason = validator.getReason(violations);
-            log.error("The provided message has invalid fields: {}", reason);
-            jmsTemplate.send("workload-dlq", session -> message);
-            return;
-        }
-
-        log.info("New workload is creating for trainer: {}", trainerDto.username());
-        trainerService.saveTrainerRecord(trainerDto);
+    public void onWorkloadMessageReceived(Message message){
+        router.routeMessage(message, TrainerDto.class, workloadDlq, trainerService::saveTrainerRecord);
     }
 }
