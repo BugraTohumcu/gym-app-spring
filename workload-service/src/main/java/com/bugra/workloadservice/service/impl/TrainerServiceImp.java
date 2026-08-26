88b854a808a181ece512033d3b45e5ca1773f10e
Ahmet Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-08-26 10:02:28 +0300

    refac: replace monthly and yearly workload creation methods with new entity

--- workload-service/src/main/java/com/bugra/workloadservice/service/impl/TrainerServiceImp.java
+++ workload-service/src/main/java/com/bugra/workloadservice/service/impl/TrainerServiceImp.java
@@ -4,16 +4,13 @@ import com.bugra.workloadservice.dto.request.TrainerDto;
 import com.bugra.workloadservice.dto.response.TrainerWorkloadResponse;
 import com.bugra.workloadservice.enums.ActionType;
 import com.bugra.workloadservice.exception.UsernameNotFoundException;
-import com.bugra.workloadservice.model.MonthlyWorkload;
 import com.bugra.workloadservice.model.Trainer;
-import com.bugra.workloadservice.model.YearlyWorkload;
 import com.bugra.workloadservice.repo.TrainerRepo;
 import com.bugra.workloadservice.service.TrainerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.time.Month;
 import java.util.HashMap;
 import java.util.Map;
@@ -30,11 +27,14 @@ public class TrainerServiceImp implements TrainerService {
         log.info("New Trainer record saving for trainer: {}", trainerDto.username());
         Trainer trainer = findOrElseCreateTrainer(trainerDto);
 
-        YearlyWorkload yearlyWorkload = findOrCreateYearlyWorkload(trainer, trainerDto.trainingDate());
+        String year = String.valueOf(trainerDto.trainingDate().getYear());
+        Month month = trainerDto.trainingDate().getMonth();
 
-        MonthlyWorkload monthlyWorkload = findOrCreateMonthlyWorkload(yearlyWorkload, trainerDto.trainingDate().getMonth());
+        Map<String, Map<String,Integer>> workload = trainer.getWorkloads();
 
-        applyDuration(monthlyWorkload, trainerDto.actionType(), trainerDto.duration());
+        Map<String, Integer> monthlyWorkload = workload.computeIfAbsent(year, k -> new HashMap<>());
+
+        applyDuration(monthlyWorkload, trainerDto.actionType(), trainerDto.duration(), month);
 
         trainerRepo.save(trainer);
     }
@@ -46,29 +46,23 @@ public class TrainerServiceImp implements TrainerService {
         Trainer trainer = trainerRepo.findByUsername(username)
                 .orElseThrow(UsernameNotFoundException::new);
 
-        Map<String, Map<String, Integer>> workloadSummary = new HashMap<>();
-
-        trainer.getWorkloads().forEach((yearKey, yearlyWorkload) -> {
-            Map<String, Integer> monthlyMap = workloadSummary.computeIfAbsent(yearKey, k -> new HashMap<>());
-
-            yearlyWorkload.getMonths().forEach((month, monthlyWorkload) ->
-                monthlyMap.merge(month, monthlyWorkload.getDuration(), Integer::sum)
-            );
-        });
-
         // create workload summary
         return TrainerWorkloadResponse.builder()
                 .username(trainer.getUsername())
                 .firstName(trainer.getFirstName())
                 .lastName(trainer.getLastName())
                 .isActive(trainer.isActive())
-                .workloads(workloadSummary)
+                .workloads(trainer.getWorkloads())
                 .build();
     }
 
-    private void applyDuration(MonthlyWorkload workload, ActionType actionType, int duration) {
+    private void applyDuration(Map<String, Integer> workload, ActionType actionType, int duration, Month month) {
+        // prevent negative duration
         int delta = actionType == ActionType.ADD ? duration : -duration;
-        workload.setDuration(Math.max(0, workload.getDuration() + delta));
+        workload.put(
+                month.name(),
+                Math.max(0, workload.getOrDefault(month.name(), 0) + delta)
+        );
     }
 
     private Trainer findOrElseCreateTrainer(TrainerDto trainerDto){
@@ -81,22 +75,5 @@ public class TrainerServiceImp implements TrainerService {
                     newTrainer.setActive(trainerDto.isActive());
                     return newTrainer;
                 });
-    }
-    private YearlyWorkload findOrCreateYearlyWorkload(Trainer trainer, LocalDate date) {
-        String year = String.valueOf(date.getYear());
-        return trainer.getWorkloads().computeIfAbsent(year, y -> {
-            YearlyWorkload yw = new YearlyWorkload();
-            yw.setYear(y);
-            yw.setTrainer(trainer);
-            return yw;
-        });
-    }
-
-    private MonthlyWorkload findOrCreateMonthlyWorkload(YearlyWorkload yearlyWorkload, Month month) {
-        return yearlyWorkload.getMonths().computeIfAbsent(month.name(), m -> {
-            MonthlyWorkload mw = new MonthlyWorkload();
-            mw.setDuration(0);
-            return mw;
-        });
     }
 }
