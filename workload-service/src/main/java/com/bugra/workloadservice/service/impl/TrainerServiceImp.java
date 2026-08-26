1520dd84e2372ec6c2f75db844bbd7ef8d6b5e7e
Ahmet Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-08-26 11:29:05 +0300

    feat: add transaction and operation logging for every method

--- workload-service/src/main/java/com/bugra/workloadservice/service/impl/TrainerServiceImp.java
+++ workload-service/src/main/java/com/bugra/workloadservice/service/impl/TrainerServiceImp.java
@@ -24,7 +24,7 @@ public class TrainerServiceImp implements TrainerService {
 
     @Override
     public void saveTrainerRecord(TrainerDto trainerDto) {
-        log.info("New Trainer record saving for trainer: {}", trainerDto.username());
+        log.info("[TRANSACTION] - New Trainer record saving for trainer: {}", trainerDto.username());
         Trainer trainer = findOrElseCreateTrainer(trainerDto);
 
         String year = String.valueOf(trainerDto.trainingDate().getYear());
@@ -32,19 +32,28 @@ public class TrainerServiceImp implements TrainerService {
 
         Map<String, Map<String,Integer>> workload = trainer.getWorkloads();
 
-        Map<String, Integer> monthlyWorkload = workload.computeIfAbsent(year, k -> new HashMap<>());
+        Map<String, Integer> monthlyWorkload = workload.computeIfAbsent(year, k -> {
+            log.info("[OPERATION] - No workload found for year {}, creating new one", year);
+            return new HashMap<>();
+        });
 
         applyDuration(monthlyWorkload, trainerDto.actionType(), trainerDto.duration(), month);
 
+        log.info("[OPERATION] - Persisting trainer record to DB for: {}", trainer.getUsername());
         trainerRepo.save(trainer);
+        log.info("[TRANSACTION] - Trainer record saved successfully for: {}", trainerDto.username());
+
     }
 
     @Override
     public TrainerWorkloadResponse getTrainerWorkload(String username) {
-        log.info("Trainer record fetching for trainer: {}", username);
+        log.info("[TRANSACTION] - Fetching workload for trainer: {}", username);
 
         Trainer trainer = trainerRepo.findByUsername(username)
-                .orElseThrow(UsernameNotFoundException::new);
+                .orElseThrow(() -> {
+                    log.info("[OPERATION] - Trainer not found for username: {}", username);
+                    return new UsernameNotFoundException();
+                });
 
         // create workload summary
         return TrainerWorkloadResponse.builder()
@@ -57,6 +66,7 @@ public class TrainerServiceImp implements TrainerService {
     }
 
     private void applyDuration(Map<String, Integer> workload, ActionType actionType, int duration, Month month) {
+        log.info("[OPERATION] - Applying the duration");
         // prevent negative duration
         int delta = actionType == ActionType.ADD ? duration : -duration;
         workload.put(
@@ -66,8 +76,11 @@ public class TrainerServiceImp implements TrainerService {
     }
 
     private Trainer findOrElseCreateTrainer(TrainerDto trainerDto){
+        log.warn("[TRANSACTION] - Fetching trainer by username: {}", trainerDto.username());
+
         return trainerRepo.findByUsername(trainerDto.username())
                 .orElseGet(() -> {
+                    log.info("[OPERATION] - Trainer not found in db. Creating new record for {}", trainerDto.username());
                     Trainer newTrainer = new Trainer();
                     newTrainer.setFirstName(trainerDto.firstName());
                     newTrainer.setLastName(trainerDto.lastName());
