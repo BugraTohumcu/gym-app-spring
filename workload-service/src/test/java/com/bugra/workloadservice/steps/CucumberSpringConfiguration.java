ff054aa86986cff7331e775788a66997551c0f4e
Ahmet Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-09-13 16:49:55 +0300

    feat: add spy bean trainer repo

--- workload-service/src/test/java/com/bugra/workloadservice/steps/CucumberSpringConfiguration.java
+++ workload-service/src/test/java/com/bugra/workloadservice/steps/CucumberSpringConfiguration.java
@@ -1,11 +1,16 @@
 package com.bugra.workloadservice.steps;
 
+import com.bugra.workloadservice.repo.TrainerRepo;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.ActiveProfiles;
 
 @CucumberContextConfiguration
 @SpringBootTest
 @ActiveProfiles("test")
 public class CucumberSpringConfiguration {
+
+    @SpyBean
+    private TrainerRepo trainerRepo;
 }
