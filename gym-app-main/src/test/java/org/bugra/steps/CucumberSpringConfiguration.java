e57de99c832d4fac1fe54067aef355ced2bb8431
Ahmet Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-09-14 11:37:56 +0300

    feat: add jms template as mockito spy bean

--- gym-app-main/src/test/java/org/bugra/steps/CucumberSpringConfiguration.java
+++ gym-app-main/src/test/java/org/bugra/steps/CucumberSpringConfiguration.java
@@ -3,11 +3,16 @@ package org.bugra.steps;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
 
 @CucumberContextConfiguration
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 public class CucumberSpringConfiguration {
+
+    @MockitoSpyBean
+    private JmsTemplate jmsTemplate;
 }
